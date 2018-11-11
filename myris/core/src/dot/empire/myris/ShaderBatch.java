c1dae4892d777bd0e446172cdf8d046e474bd202
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2018-11-11 14:03:44 +0200

    Added shader batch
    Not implemented yet.
    https://github.com/mattdesl/lwjgl-basics/wiki/LibGDX-Brightness-&-Contrast

---
+++ myris/core/src/dot/empire/myris/ShaderBatch.java
@@ -0,0 +1,116 @@
+package dot.empire.myris;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+
+/**
+ *
+ */
+public final class ShaderBatch extends SpriteBatch {
+
+    private static final String vertexShader = "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+            + "attribute vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+            + "attribute vec2 " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+            + "uniform mat4 u_projTrans;\n" //
+            + "varying vec4 v_color;\n" //
+            + "varying vec2 v_texCoords;\n" //
+            + "\n" //
+            + "void main()\n" //
+            + "{\n" //
+            + "   v_color = " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+            + "   v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+            + "   gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+            + "}\n";
+
+    private static final String fragmentShader = "#ifdef GL_ES\n" //
+            + "#define LOWP lowp\n" //
+            + "precision mediump float;\n" //
+            + "#else\n" //
+            + "#define LOWP \n" //
+            + "#endif\n" //
+            + "varying LOWP vec4 v_color;\n" //
+            + "varying vec2 v_texCoords;\n" //
+            + "uniform sampler2D u_texture;\n" //
+            + "uniform float brightness;\n" //
+            + "uniform float contrast;\n" //
+            + "void main()\n"//
+            + "{\n" //
+            + "  vec4 color = v_color * texture2D(u_texture, v_texCoords);\n"
+            + "  color.rgb /= color.a;\n" //ignore alpha
+            + "  color.rgb = ((color.rgb - 0.5) * max(contrast, 0.0)) + 0.5;\n" //apply contrast
+            + "  color.rgb += brightness;\n" //apply brightness
+            + "  color.rgb *= color.a;\n" //return alpha
+            + "  gl_FragColor = color;\n"
+            + "}";
+
+    public final boolean isCompiled;
+    public final String log;
+    public float brightness;
+    public float contrast;
+    protected int brightnessLoc = -1, contrastLoc = -1;
+    ShaderProgram shader;
+
+    public ShaderBatch(int size) {
+        super(size);
+
+        this.contrast = 1f;
+        this.brightness = 0;
+
+        ShaderProgram.pedantic = false;
+        shader = new ShaderProgram(vertexShader, fragmentShader);
+        isCompiled = shader.isCompiled();
+        log = shader.getLog();
+
+        if (isCompiled) {
+            setShader(shader);
+            {
+                this.shader.begin();
+                this.brightnessLoc = shader.getUniformLocation("brightness");
+                this.contrastLoc = shader.getUniformLocation("contrast");
+            }
+            this.shader.end();
+        }
+    }
+
+    @Override
+    public void begin() {
+        super.begin();
+        if (shader != null) {
+            if (brightnessLoc != -1) {
+                this.shader.setUniformf(brightnessLoc, brightness);
+            }
+            if (contrastLoc != -1) {
+                this.shader.setUniformf(contrastLoc, contrast);
+            }
+        }
+    }
+
+    public float getBrightness() {
+        return this.brightness;
+    }
+
+    public void setBrightness(float brightness) {
+        this.brightness = brightness;
+    }
+
+    public float getContrast() {
+        return this.contrast;
+    }
+
+    public void setContrast(float contrast) {
+        this.contrast = contrast;
+    }
+
+//    @Override
+//    public String toString() {
+//        return MoreObjects.toStringHelper(this)
+//                .add("isCompiled", isCompiled)
+//                .add("log", log)
+//                .add("brightness", brightness)
+//                .add("contrast", contrast)
+//                .add("brightnessLoc", brightnessLoc)
+//                .add("contrastLoc", contrastLoc)
+//                .add("shader", shader)
+//                .toString();
+//    }
+}
