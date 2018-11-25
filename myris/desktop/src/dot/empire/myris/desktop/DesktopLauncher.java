bc1ae17fbf2dcdcbc43c15c4a342893656fd6a65
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2018-11-25 15:47:22 +0200

    Recreated libGDX project

---
+++ myris/desktop/src/dot/empire/myris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dot.empire.myris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dot.empire.myris.Myris;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Myris(), config);
+	}
+}
